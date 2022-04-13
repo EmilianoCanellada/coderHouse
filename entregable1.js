e75a0403824a8a27df7754a0ce9181bac70dba99
EmilianoCanelalda <canelladaemiliano@gmail.com>
2022-04-13 20:30:54 -0300

    Terminado

--- entregable1.js
+++ entregable1.js
@@ -1,7 +1,8 @@
-document.querySelector('#calcular').addEventListener('click', () =>{      //Elemento a escuchar para calcular; Al darle click en calcular se inidica la arroy function que recoge valores
-    const numero1 = parseInt(document.querySelector('#numero1').value);   //El elemento parseInt convierte a un numero entero
-    const numero2 = parseInt(document.querySelector('#numero2').value);
-    const operacion = document.querySelector('#operacion').value;
+document.getElementById('calcular').addEventListener('click', () =>{      //Elemento a escuchar para calcular; Al darle click en calcular se inidica la arroy function que recoge valores
+    const numero1 = parseInt(document.getElementById('numero1').value);   //El elemento parseInt convierte a un numero entero
+    const numero2 = parseInt(document.getElementById('numero2').value);
+    const operacion = document.getElementById('operacion').value;
+    const resultadoHtml = document.getElementById('resultado');
 
     let resultado;
     if (operacion == '+') {                              //Si la operacion es una suma, suma
@@ -17,6 +18,5 @@ document.querySelector('#calcular').addEventListener('click', () =>{      //Elem
         resultado = numero1 / numero2; 
 
     }
-    //document.querySelector('#r').innerHTML = r;  --------> NO PUEDO HACERLO ANDAR
-    console.log(resultado);
+   resultadoHtml.innerHTML = ' " ' + resultado + ' " ';
 })
