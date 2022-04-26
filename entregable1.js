a706e0b2614b639248700d12f7e120140d588968
EmilianoCanelalda <canelladaemiliano@gmail.com>
2022-04-26 19:01:05 -0300

    Primera entrega del Proyecto Final

--- entregable1.js
+++ entregable1.js
@@ -18,10 +18,11 @@ document.getElementById('calcular').addEventListener('click', () =>{      //Elem
         resultado = numero1 / numero2; 
 
     }
-   resultadoHtml.innerHTML = ' " ' + resultado + ' " ';
+    let result = resultadoHtml.innerHTML = ' " ' + resultado + ' " ';
+    guardar(result);
 });
- function guardar(){
-    function ResultadoOperacion(numero1, operacion,numero2){ //cada vez que se inicie el objeto y se coloquen los datos se va a transformar en el objeto que estamos definiendo (es un constructor de objetos)
+ function guardar(result){
+    function ResultadoOperacion(numero1, operacion, numero2, resultado){ //cada vez que se inicie el objeto y se coloquen los datos se va a transformar en el objeto que estamos definiendo (es un constructor de objetos)
         this.numero1 = numero1;
         this.operacion = operacion;
         this.numero2 = numero2;
@@ -30,15 +31,15 @@ document.getElementById('calcular').addEventListener('click', () =>{      //Elem
     let numero1Guardar = document.getElementById("numero1").value; //capturo los datos que se guardan en la variable numero1Guardar
     let operacionGuardar = document.getElementById("operacion").value; //capturo los datos que se guardan en la variable operacionGuardar
     let numero2Guardar = document.getElementById("numero2").value; //capturo los datos que se guardan en la variable numero2Guardar
-    let resultadoHtml = document.getElementById("resultado").value; //capturo los datos que se guardan en la variable resultadoHtml
+    //let resultadoHtml = document.getElementById("resultado").innerHTML; //capturo los datos que se guardan en la variable resultadoHtml
 
 
-    resultadoTotal = new ResultadoOperacion(numero1Guardar, operacionGuardar, numero2Guardar, resultadoHtml)    //creando un nuevo objeto llado "ResultadoOperacion" donde almacenara el resultado total
-    guardarResultado();
+    resultadoTotal = new ResultadoOperacion(numero1Guardar, operacionGuardar, numero2Guardar, result)    //creando un nuevo objeto llado "ResultadoOperacion" donde almacenara el resultado total
+    guardarResultado(resultadoTotal);
 };
     let resultadoBD = [];
-    function guardarResultado (){
-        resultadoBD.push(resultadoTotal) //Agrego al arrays resultadoBD los datos del objeto que creamos arriba (no lleva "var" ni "let" porque es una variable global)
-        document.getElementById("tabla").innerHTML += '<tbody><td>'+resultadoTotal.numero1+'</td><td>'+resultadoTotal.operacion+'</td><td>'+resultadoTotal.numero2+'</td><td>'+resultadoTotal.resultadoHtml+'</td></tbody>';
+    function guardarResultado (resultado){
+        resultadoBD.push(resultado) //Agrego al arrays resultadoBD los datos del objeto que creamos arriba (no lleva "var" ni "let" porque es una variable global)
+        document.getElementById("tabla").innerHTML += '<tbody><td>'+resultado.numero1+'</td><td>'+resultado.operacion+'</td><td>'+resultado.numero2+'</td><td>'+resultado.resultado+'</td></tbody>';
     //Detecta la tabla e inserta html con etiquetas propias de una tabla ("https://getbootstrap.com/docs/4.0/content/tables/")
     };
